5b33e1377af01d4ec3475881544c77d5d59a5182
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2024-05-22 18:00:42 +0200

    Started working on contact, installed form hook

--- ecom-store/src/Contact.jsx
+++ ecom-store/src/Contact.jsx
@@ -1,10 +1,45 @@
-import React from "react";
+/* 
+- Full name (Minimum number of characters is 3, required)
+- Subject (Minimum number of characters is 3, required)
+- Email (Must be a valid email address, required)
+- Body (Minimum number of characters is 3, required)
+- Submit button */
 
-function Contact() {
-  <div>
-    <h2>Contact page</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, eaque!</p>
-  </div>;
+// React hook form utilised: https://content.noroff.dev/front-end-frameworks/react-hook-form.html
+import React from "react";
+import { useForm } from "react-hook-form";
+
+function ReactHookForm() {
+  // handleSubmit receives the form data if validation is succs.
+  // register registers input by creating a ref
+  const { register, handleSubmit } = useForm();
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
+  return (
+    <div>
+      <form>
+        {/* Fullname  */}
+        <input
+          {...register("fullName", {
+            required: true,
+            minLength: 3,
+            maxLength: 50,
+          })}
+        />
+        {/* Subject  */}
+        <input
+          {...register("subject", {
+            required: true,
+            minLength: 3,
+            maxLength: 100,
+          })}
+        />
+      </form>
+    </div>
+  );
 }
 
-export default Contact;
+export default ReactHookForm;
