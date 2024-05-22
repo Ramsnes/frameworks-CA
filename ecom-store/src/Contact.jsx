1552aec7ec3c7c542bac6090b82cb8f151d8f9cc
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2024-05-22 23:28:21 +0200

    adds draft contact page

--- ecom-store/src/Contact.jsx
+++ ecom-store/src/Contact.jsx
@@ -20,23 +20,62 @@ function ReactHookForm() {
 
   return (
     <div>
-      <form>
-        {/* Fullname  */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* Full name  */}
+        <label htmlFor="fullName">Full Name:</label>
         <input
           {...register("fullName", {
             required: true,
             minLength: 3,
             maxLength: 50,
           })}
+          id="fullName"
+          placeholder="Minimum 3 characters"
         />
+
         {/* Subject  */}
-        <input
+        <label htmlFor="subject">Subject:</label>
+        <select
           {...register("subject", {
             required: true,
             minLength: 3,
             maxLength: 100,
           })}
+          id="subject"
+        >
+          <option value={"complaint"}>Complaint</option>
+          <option value={"other"}>Other</option>
+        </select>
+
+        {/* Email  */}
+        <label htmlFor="email">Email:</label>
+        <input
+          {...register("email", {
+            required: true,
+            pattern: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+          })}
+          id="email"
+          placeholder="E.g name@domain.com"
         />
+        {/* regex check: 
+Starting with one or more letters, numbers, periods, underscores, plus signs, or hyphens ([A-Za-z0-9._%+-]+)
+Followed by "@" symbol (@)
+Then one or more letters, numbers, periods, or hyphens in the domain name ([A-Za-z0-9.-]+)
+Ending with a top-level domain (TLD) with at least two characters ([A-Za-z]{2,})
+ */}
+
+        {/* Body  */}
+        <label htmlFor="body">Body:</label>
+        <input
+          {...register("body", {
+            required: true,
+            minLength: 3,
+            maxLength: 100,
+          })}
+          id="body"
+          placeholder="Minimum 3 characters"
+        />
+        <input type="submit" />
       </form>
     </div>
   );
